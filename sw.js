2872feb00bd679453959efdbe483c02d999bbd46
witek746653-maker <witek746653@gmail.com>
2025-09-03 16:32:25 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,27 +1,41 @@
-const CACHE = "sabor-pwa-v1";
+// Sabor PWA service worker
+const CACHE = 'sabor-pwa-v9';
 const ASSETS = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './sw.js',
+  './photo.png',
+  './icons/rrrrr-180.png',
+  './icons/rrrrr-192.png',
+  './icons/rrrrr-512.png'
 ];
-self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
-  self.skipWaiting();
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
 });
-self.addEventListener("activate", (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE && caches.delete(k))))
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
   );
   self.clients.claim();
 });
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request).then(resp => {
-      const copy = resp.clone();
-      caches.open(CACHE).then(cache => cache.put(e.request, copy)).catch(()=>{});
-      return resp;
-    }).catch(()=> caches.match("./index.html")))
-  );
+
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (url.origin === location.origin && (url.pathname.endsWith('.html') || url.pathname === '/')) {
+    event.respondWith(
+      fetch(event.request).then(resp => {
+        const copy = resp.clone();
+        caches.open(CACHE).then(c => c.put(event.request, copy));
+        return resp;
+      }).catch(() => caches.match(event.request))
+    );
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request)));
 });
