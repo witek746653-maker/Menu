54624c2fa8c1f38f97573cb3a4de1499ca37bf59
Твоё Имя <witek746653@gmail.com>
2026-02-01 15:04:07 +0300

    онбординг добавил

--- start_server.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd /d %~dp0
-echo ================================
-echo   🚀 Запускаю локальный сервер...
-echo ================================
-start "" cmd /k "python server.py"
-timeout /t 2 >nul
-start "" http://localhost:8000/index.html
-pause
